7b164a2a4c5e0524dcf892a010b2a6dbf6e60a5f
Leonid Konchulizov <leonid.konchulizov@tripster.ru>
2025-04-19 00:14:55 +0300

    feat: ui auth registration jobseeker about-me

---
+++ src/pages/auth/registration/jobseeker/about-me/index.route-record.ts
@@ -0,0 +1,7 @@
+import { currentRoute } from "./model.ts";
+import { AuthRegistrationJobseekerAboutMePage } from "./ui.tsx";
+
+export default {
+  view: AuthRegistrationJobseekerAboutMePage,
+  route: currentRoute,
+};
