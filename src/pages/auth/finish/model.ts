a161fd7c97896089ad9ae9fd6fdab82413ddf9b8
Leonid Konchulizov <leonid.konchulizov@tripster.ru>
2025-04-27 10:10:28 +0300

    feat: sign in finish

--- src/pages/auth/finish/model.ts
+++ src/pages/auth/finish/model.ts
@@ -1,3 +1,74 @@
+import { createEvent, createStore, sample } from "effector";
+import { interval, reset } from "patronum";
+
+import { getMeQuery } from "@/shared/api/user";
 import { routes } from "@/shared/routing";
+import { UserRole } from "@/shared/types/user.interface.ts";
 
 export const currentRoute = routes.auth.finish;
+
+export const startCountdown = createEvent();
+export const stopCountdown = createEvent();
+
+export const $countdownSeconds = createStore(4);
+export const $countdownFinished = createStore(false);
+export const $queryFinished = createStore(false);
+
+const { tick } = interval({
+  timeout: 1_000, // 1 секунда
+  start: startCountdown,
+  stop: stopCountdown,
+});
+
+$countdownSeconds.on(tick, (count) => Math.max(0, count - 1));
+
+sample({
+  source: $countdownSeconds,
+  filter: (count) => count === 0,
+  fn: () => true,
+  target: [$countdownFinished, stopCountdown],
+});
+
+sample({
+  clock: currentRoute.opened,
+  target: [getMeQuery.start, startCountdown],
+});
+
+$queryFinished.on(getMeQuery.finished.success, () => true);
+
+const $readyToRedirect = createStore(false)
+  .on($countdownFinished, (_, countdownFinished) => countdownFinished && $queryFinished.getState())
+  .on($queryFinished, (_, queryFinished) => queryFinished && $countdownFinished.getState());
+
+sample({
+  clock: $readyToRedirect,
+  source: getMeQuery.finished.success,
+  filter: (response, ready) =>
+    ready &&
+    Boolean(
+      response.result.user &&
+        response.result.user.role === UserRole.Jobseeker &&
+        response.result.jobseeker,
+    ),
+  fn: ({ result }) => ({ jobseekerId: result.jobseeker!._id }),
+  target: routes.jobseeker.search.open,
+});
+
+sample({
+  clock: $readyToRedirect,
+  source: getMeQuery.finished.success,
+  filter: (response, ready) =>
+    ready &&
+    Boolean(
+      response.result.user &&
+        response.result.user.role === UserRole.Company &&
+        response.result.company,
+    ),
+  fn: ({ result }) => ({ companyId: result.company!._id }),
+  target: routes.company.search.open,
+});
+
+reset({
+  clock: currentRoute.closed,
+  target: [$countdownSeconds, $countdownFinished, $queryFinished, $readyToRedirect],
+});
--- src/pages/auth/finish/ui.tsx
+++ src/pages/auth/finish/ui.tsx
@@ -1,6 +1,12 @@
+import { useUnit } from "effector-react";
+
+import { $countdownSeconds } from "@/pages/auth/finish/model.ts";
+
 import { LogoLink } from "@/shared/ui/logo-link.tsx";
 
 export const AuthRegistrationFinishPage = () => {
+  const countdown = useUnit($countdownSeconds);
+
   return (
     <div className="flex flex-col min-h-screen">
       <header className="flex justify-between items-center p-4 bg-background/80 backdrop-blur-sm z-10">
@@ -12,6 +18,9 @@ export const AuthRegistrationFinishPage = () => {
           <div className="w-full max-w-md p-6 space-y-6 bg-card rounded-lg shadow-md text-center">
             <div className="space-y-4">
               <h1 className="text-2xl font-bold">Вы успешно зарегистрировались!</h1>
+              <p className="text-sm text-muted-foreground">
+                перенаправим вас на главную страницу через {countdown} секунд
+              </p>
             </div>
           </div>
         </div>
